1325cfa964e94d0a6d418ab91b21f553f1f93db4
ghostrabbir0703 <rummypa123@gmail.com>
2026-07-13 01:35:53 -0400

    funcionalidad botones y checkbox

--- scripts.js
+++ scripts.js
@@ -5,6 +5,11 @@ document.addEventListener('DOMContentLoaded',function(){
     let descriptionTask=document.getElementById("description-task")
     let buttonCreateTask=document.getElementById("button-create-task");
     let incrementId=1;
+    const areaListTask=document.querySelector(".space-crud-task");
+        areaListTask.innerHTML="";
+        
+
+
     
     function createTask(nameTask ,descriptionTask){
         const nameTaskValue = nameTask.value;
@@ -17,7 +22,8 @@ document.addEventListener('DOMContentLoaded',function(){
         const task={
             id:incrementId,
             name: nameTaskValue,          
-            description: descriptionTaskValue
+            description: descriptionTaskValue,
+            checked: false
         };
         listTasks.push(task)
 
@@ -27,59 +33,169 @@ document.addEventListener('DOMContentLoaded',function(){
         incrementId++;
         alert("su registro fue exitoso");
         renderListTask();
+        location.reload();
         
     }
 
     function renderListTask(){
-        const areaListTask=document.querySelector(".space-crud-task");
-        areaListTask.innerHTML="";
+        
         listTasks.forEach(item=>{
-            const rowListTask=document.createElement("div")
-            const nameTaskElement=document.createElement("div");
-            const descriptionTaskElement=document.createElement("div");
+            const rowListTask=document.createElement("div");
+            const contentTaskElement=document.createElement("div");
+            const checkBoxTaskElement=document.createElement("input");
+            const textTaskElement = document.createElement("div");
+            const nameTaskElement=document.createElement("h3");
+            const descriptionTaskElement=document.createElement("p");
             const actionTaskElement=document.createElement("div");
             const buttonView=document.createElement("button");
             const buttonEdit=document.createElement("button");
             const buttonDelete=document.createElement("button");
 
 
+            checkBoxTaskElement.type = "checkbox";
+
             rowListTask.classList.add("row-list-task");
+            checkBoxTaskElement.classList.add("checkbox-list-task");
+            contentTaskElement.classList.add("content-task");
+            textTaskElement.classList.add("text-task");
             nameTaskElement.classList.add("name-task-list");
-            descriptionTask.classList.add("description-task-list");
+            descriptionTaskElement.classList.add("description-task-list");
             actionTaskElement.classList.add("action-task-list");
-            buttonView.id="button-view-task";
-            buttonEdit.id="button-edit-task";
-            buttonDelete.id="button-delete-task";
+
+            buttonView.classList.add("button-view-task");
+            buttonEdit.classList.add("button-edit-task");
+            buttonDelete.classList.add("button-delete-task"); 
+
+            buttonView.dataset.taskId=item.id;
+            buttonEdit.dataset.taskId = item.id;
+            buttonDelete.dataset.taskId = item.id;
 
             nameTaskElement.textContent=`${item.name}`;
             descriptionTaskElement.textContent=`${item.description}`;
-            buttonView.textContent="Ver"
-            buttonEdit.textContent="Editar"
-            buttonDelete.textContent="Eliminar"
+            buttonView.textContent="Ver";
+            buttonEdit.textContent="Editar";
+            buttonDelete.textContent="Eliminar";
+
+            checkBoxTaskElement.addEventListener("change", () => {
+
+                if (checkBoxTaskElement.checked) {
+                    nameTaskElement.style.textDecoration = "line-through";
+                    descriptionTaskElement.style.textDecoration ="line-through";
+                } else {
+                    nameTaskElement.style.textDecoration = "none";
+                    descriptionTaskElement.style.textDecoration ="none";               
+                }
+
+            });
+
+            buttonView.addEventListener("click", () => {
+                viewTask(item.id);
+            });
+
+            buttonEdit.addEventListener("click", () => {
+                editTask(item.id);
+            });
+
+            buttonDelete.addEventListener("click", () => {
+                deleteTask(item.id);
+            });
+
+            textTaskElement.appendChild(nameTaskElement);
+            textTaskElement.appendChild(descriptionTaskElement);
+
+            contentTaskElement.appendChild(checkBoxTaskElement);
+            contentTaskElement.appendChild(textTaskElement);
 
             actionTaskElement.appendChild(buttonView);
             actionTaskElement.appendChild(buttonEdit);
             actionTaskElement.appendChild(buttonDelete);
 
-
-            rowListTask.appendChild(nameTaskElement);
-            rowListTask.appendChild(descriptionTaskElement);
+            rowListTask.appendChild(contentTaskElement);
             rowListTask.appendChild(actionTaskElement);
 
             areaListTask.appendChild(rowListTask);
             areaListTask.appendChild(document.createElement("hr"))
         })
     }
+
+
+    function viewTask(id) {
+        const taskFound = listTasks.find(task => task.id === id);
+        if (!taskFound) {
+            alert("No se encontró la tarea");
+            return;
+        }
+
+        alert(
+            `Nombre: ${taskFound.name}\nDescripción: ${taskFound.description}`
+        );
+    }
+
+    function editTask(id){
+        const taskFound = listTasks.find(task => task.id === id);
+        if (!taskFound) {
+            alert("No se encontró la tarea");
+            return;
+        }
+        
+        let newNameTask=prompt(`ingresa el nuevo valor para la tarea: ${taskFound.name}`);
+        let newDescriptionTask=prompt(`ingresa el nuevo valor para la descripcion: ${taskFound.description}`);
+
+        if (newNameTask === null || newDescriptionTask === null) {
+            return;
+        }
+
+        if (!newNameTask.trim() || !newDescriptionTask.trim()) {
+            alert("Los campos no pueden estar vacíos");
+            return;
+        }
+
+        taskFound.name = newNameTask.trim();
+        taskFound.description = newDescriptionTask.trim();
+        localStorage.setItem("Tasks", JSON.stringify(listTasks));
+
+        renderListTask();
+        location.reload();
+
+        //alert("se actualizo la tarea");
+        
+    }
+
+
+    function deleteTask(id) {
+    const taskIndex = listTasks.findIndex(task => task.id === id);
+
+        if (taskIndex === -1) {
+            alert("No se encontró la tarea");
+            return;
+        }
+
+        listTasks.splice(taskIndex, 1);
+
+        localStorage.setItem("Tasks", JSON.stringify(listTasks));
+
+        renderListTask();
+        alert("se elimino el registro ")
+        location.reload();
+        
+    }
+
     renderListTask();
 
-      if (listTasks.length > 0) {
-            countOperation = listTasks[listTasks.length - 1].number + 1;
-        };
+    
   
     buttonCreateTask.addEventListener('click',()=>{
         createTask( nameTask,descriptionTask);
     })
+
+
+    if (listTasks.length > 0) {
+            incrementId = Math.max(...listTasks.map(task => task.id)) + 1;
+    }
+
+    
     //localStorage.clear();
+
 
 })
 
